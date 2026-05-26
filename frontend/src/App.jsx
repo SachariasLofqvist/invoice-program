222aa5e3b042216cb49698ff1bfaff382fc33193
Sacharias Löfqvist <lofqvist.sacharias@gmail.com>
2026-05-26 12:40:14 +0200

    Reverted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     <>
       <SignedIn>
         <BrowserRouter>
-          <div className="absolute top-4 right-4 z-50 hide-on-print">
+          <div className="absolute top-4 right-4 z-50">
             <UserButton/>
           </div>
           
