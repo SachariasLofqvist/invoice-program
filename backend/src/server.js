f0a99a55547bc48600b44d68183f043ac7706b2e
Sacharias Löfqvist <lofqvist.sacharias@gmail.com>
2026-05-31 20:25:47 +0200

    reverted

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_API],
+  origin: process.env.FRONTEND_URL,
   optionsSuccessStatus: 200,
   credentials: true,
 };
