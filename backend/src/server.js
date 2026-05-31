825c8047d0f18b36310f0f4e5040347e8838d186
Sacharias Löfqvist <lofqvist.sacharias@gmail.com>
2026-05-31 16:11:38 +0200

    added cors options

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,22 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import invoiceRoutes from './routes/invoiceRoutes.js'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import invoiceRoutes from "./routes/invoiceRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/api/invoices', invoiceRoutes)
+app.use("/api/invoices", invoiceRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server körs på http://localhost:${PORT}`);
-})
+  console.log(`Server körs på http://localhost:${PORT}`);
+});
